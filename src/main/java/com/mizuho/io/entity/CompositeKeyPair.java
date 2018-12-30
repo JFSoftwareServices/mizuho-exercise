8b1421015309d082f075e8381697f34dda6c5477
Kanke <sugarkanke@yahoo.com>
2018-12-30 19:02:50 +0000

    Reverting some code changes

--- src/main/java/com/mizuho/io/entity/CompositeKeyPair.java
+++ src/main/java/com/mizuho/io/entity/CompositeKeyPair.java
@@ -2,6 +2,8 @@ package com.mizuho.io.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -11,10 +13,26 @@ import static java.util.Comparator.comparing;
 /**
  * The key used in the cache will be a combined key consisting  of ticker and vendor
  */
-@Data @AllArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class CompositeKeyPair implements Comparable<CompositeKeyPair>, Serializable {
     private String ticker;
     private String vendor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CompositeKeyPair that = (CompositeKeyPair) o;
+        return Objects.equals(ticker, that.ticker) &&
+                Objects.equals(vendor, that.vendor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ticker, vendor);
+    }
 
     @Override
     public int compareTo(CompositeKeyPair o) {
--- src/main/java/com/mizuho/io/entity/InstrumentEntity.java
+++ src/main/java/com/mizuho/io/entity/InstrumentEntity.java
@@ -1,13 +1,17 @@
 package com.mizuho.io.entity;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 
-@Data @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class InstrumentEntity implements Serializable {
     private BigDecimal price;
     private String ticker;
@@ -22,4 +26,36 @@ public class InstrumentEntity implements Serializable {
         this.date = date;
         compositeKeyPair = new CompositeKeyPair(ticker, vendor);
     }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * The other object is considered to be equal to this one if
+     * <p>
+     * The other object is considered equal to this one if:
+     * <ul>
+     * <li>The price of the other object is equal to this one
+     * <li>The ticker of the other object is equal to this one
+     * <li>The vendor of the other object is equal to this one
+     * </ul>
+     * <p>
+     *
+     * @param o an object
+     * @return {@code true} if the price and ticker and vendor of this object is
+     * equal to the price and ticker and vendor of the object
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InstrumentEntity that = (InstrumentEntity) o;
+        return Objects.equals(price, that.price) &&
+                Objects.equals(ticker, that.ticker) &&
+                Objects.equals(vendor, that.vendor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price, ticker, vendor);
+    }
+
 }
